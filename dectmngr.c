c7a78b9791ea6e1f779b62eb67cbcf61cc73e821
Jonas Höglund <jonas.hoglund@southpole.se>
2013-02-05 14:49:57 +0100

    Added endpoint init command for dect

--- dectmngr.c
+++ dectmngr.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <dectshimdrv.h>
 #include <ApiFpProject.h>
+#include <endpointdrv.h>
 
 
 /* globals */
@@ -23,7 +24,7 @@ extern unsigned char DectNvsData[];
 #define API_FILE "/dev/dect"
 
 #define API_FP_MM_SET_REGISTRATION_MODE_REQ 0x4105
-
+#define EPSTATUS_DRIVER_ERROR       EPSTATUS_UNKNOWN_TYPE
 
 
 
@@ -107,425 +108,6 @@ int ping_handsets(void) {
 }
 
 
-/* Do we need to do this? */
-
-/* int dectCtlImplHwInit(void) */
-/* { */
-/*    int status = 0; */
-/*    int fileHandle; */
-/*    DECTSHIMDRV_INIT_PARAM parm; */
-
-/*    fileHandle = open("/dev/dectshim", O_RDWR); */
-
-/*    if( fileHandle == -1 ) */
-/*    { */
-/*       printf( "%s: open error %d\n", __FUNCTION__, errno ); */
-/*       return -1; */
-/*    } */
-/*    else if(( status = ioctl( fileHandle, DECTSHIMIOCTL_INIT_CMD, &parm )) != 0 ) */
-/*    { */
-/*       cmsLog_error("%s: error during ioctl", __FUNCTION__); */
-/*    } */
-
-/*    close(fileHandle); */
-
-/*    // *** Auto unload the module */
-/*    // grep dect /proc/modules &>/dev/null ||  - no grep on the system */
-/*    status = system("insmod /lib/modules/2.6.30/extra/dect.ko 2>/dev/null"); */
-/*    if (status == -1) */
-/*    { */
-/*       cmsLog_error("Module load command failed"); */
-/*    } */
-/*    else if (WEXITSTATUS(status)) */
-/*    { */
-/*       cmsLog_error("dect.ko module not loaded (already loaded or not found)"); */
-/*    } */
-/*    else */
-/*    { */
-/*       cmsLog_notice("dect kernel module auto loaded"); */
-/*    } */
-/*    return( status ); */
-/* } */
-
-
-
-
-/****************************************************************************
-*
-*  FUNCTION:   dectCtlImplEepromCmd
-*
-*  PURPOSE:    Send EEPROM cmd to the DECT base module
-*
-*  PARAMETERS:
-*     cmd     - Get or Set command
-*     address - Address of EEPROM
-*     length  - data length
-*     data    - New data for Set operation
-*  RETURNS:
-*     Nothing
-*
-*  NOTES:
-*
-****************************************************************************/
-/* void dectCtlImplEepromCmd(DECT_EEPROMCMD_TYPE cmd, int address, int length, int data ) */
-/* { */
-
-/*    unsigned char *tempPtr = NULL; */
-/*    switch (cmd) */
-/*    { */
-/*       case DECT_EEPROMCMD_GET_DATA: */
-/*       { */
-/*          tempPtr = (unsigned char*) malloc (7); */
-/*          if (tempPtr == NULL) */
-/*          { */
-/*             cmsLog_error("No more memory!!!"); */
-/*             return; */
-/*          } */
-/*          *(tempPtr+0) = 0; // Length MSB */
-/*          *(tempPtr+1) = 5; // Length LSB */
-/*          *(tempPtr+2) = (unsigned char) ((API_FP_GET_EEPROM_REQ&0xff00)>>8);  // Primitive MSB */
-/*          *(tempPtr+3) = (unsigned char)  (API_FP_GET_EEPROM_REQ&0x00ff);      // Primitive LSB */
-/*          *(tempPtr+4) = (unsigned char)   (address&0x00ff);                   // EEPROM Address */
-/*          *(tempPtr+5) = (unsigned char)  ((address&0xff00)>>8);               // EEPROM Address */
-/*          *(tempPtr+6) = (unsigned char)   length;                             // Command Length */
-/*       } */
-/*       break; */
-/*       case DECT_EEPROMCMD_SET_DATA: */
-/*       { */
-/*          tempPtr = (unsigned char*) malloc (8); */
-/*          if (tempPtr == NULL) */
-/*          { */
-/*             cmsLog_error("No more memory!!!"); */
-/*             return; */
-/*          } */
-/*          *(tempPtr+0) = 0; // Length MSB */
-/*          *(tempPtr+1) = 6; // Length LSB */
-/*          *(tempPtr+2) = (unsigned char) ((API_FP_SET_EEPROM_REQ&0xff00)>>8);  // Primitive MSB */
-/*          *(tempPtr+3) = (unsigned char)  (API_FP_SET_EEPROM_REQ&0x00ff);      // Primitive LSB */
-/*          *(tempPtr+4) = (unsigned char)  (address&0x00ff);                    // EEPROM Address */
-/*          *(tempPtr+5) = (unsigned char) ((address&0xff00)>>8);                // EEPROM Address */
-/*          *(tempPtr+6) = (unsigned char)   length;                             // Command Length */
-/*          *(tempPtr+7) = (unsigned char)   data;                               // Command Data */
-/*       } */
-/*       break; */
-/*       default: */
-/*       break; */
-/*    } */
-
-/*    if (tempPtr != NULL) */
-/*    { */
-/*       bosMutexAcquire(&semMailFrameQueueSem); */
-/*       EnQueue(&mailFrameQueue, tempPtr); */
-/*       bosMutexRelease(&semMailFrameQueueSem); */
-/*    } */
-/* } */
-
-/****************************************************************************
-*
-*  FUNCTION:   dectCtlImplRegistrationCmd
-*
-*  PURPOSE:    Send Registration cmd to the DECT base module
-*
-*  PARAMETERS:
-*     cmd  - Registration command
-*     dataPtr - pointer to data related with each command
-*     length - data length
-*  RETURNS:
-*     Nothing
-*
-*  NOTES:
-*
-****************************************************************************/
-/* void dectCtlImplRegistrationCmd(DECT_REGCMD_TYPE cmd, unsigned char* dataPtr, int length) */
-/* { */
-/*    unsigned char *tempPtr = NULL; */
-
-/*    switch (cmd) */
-/*    { */
-/*       /\* case DECT_REGCMD_DELETE_HANDSET: *\/ */
-/*       /\* { *\/ */
-/*       /\*    if (length < 1) *\/ */
-/*       /\*    { *\/ */
-/*       /\*       cmsLog_error("No handset is specify to delete!"); *\/ */
-/*       /\*    } *\/ */
-
-/*       /\*    tempPtr = (unsigned char*) malloc(5); *\/ */
-/*       /\*    if (tempPtr == NULL) *\/ */
-/*       /\*    { *\/ */
-/*       /\*       cmsLog_error("No more memory!!!"); *\/ */
-/*       /\*       return; *\/ */
-/*       /\*    } *\/ */
-
-/*       /\*    *(tempPtr+0) = 0; // Length MSB *\/ */
-/*       /\*    *(tempPtr+1) = 3; // Length LSB *\/ */
-/*       /\*    *(tempPtr+2) = (unsigned char) ((API_FP_MM_DELETE_REGISTRATION_REQ&0xff00)>>8); // Primitive MSB *\/ */
-/*       /\*    *(tempPtr+3) = (unsigned char) (API_FP_MM_DELETE_REGISTRATION_REQ&0x00ff);      // Primitive LSB *\/ */
-/*       /\*    *(tempPtr+4) = (unsigned char) dataPtr[0];      // Handset Id *\/ */
-/*       /\* } *\/ */
-/*       /\* break; *\/ */
-
-/*       case DECT_REGCMD_PING_HANDSET: */
-/*       { */
-/*          if (length < 1) */
-/*          { */
-/*             cmsLog_error("No handset specifed to ping!"); */
-/*             return; */
-/*          } */
-
-/*          int handset = (int)dataPtr[0]; */
-/*          /\* if the handset ID is 0, we want to ping all the handsets *\/ */
-/*          if( handset == 0 ) */
-/*          { */
-/*             int temp; */
-/*             /\* set each ping timeout *\/ */
-/*             for( temp = 0; temp < DECT_MAX_HANDSET ; temp++ ) */
-/*             { */
-/*                pingHandset[temp] = TRUE; */
-/*             } */
-/*          } */
-/*          else */
-/*          { */
-/*             pingHandset[handset-1] = TRUE; */
-/*          } */
-/*       } */
-/*       break; */
-
-/*       case DECT_REGCMD_START_REG: */
-/*       { */
-/*          tempPtr = (unsigned char*) malloc(5); */
-/*          if (tempPtr == NULL) */
-/*          { */
-/*             cmsLog_error("No more memory!!!"); */
-/*             return; */
-/*          } */
-/*          *(tempPtr+0) = 0; // Length MSB */
-/*          *(tempPtr+1) = 3; // Length LSB */
-/*          *(tempPtr+2) = (unsigned char) ((API_FP_MM_SET_REGISTRATION_MODE_REQ&0xff00)>>8); // Primitive MSB */
-/*          *(tempPtr+3) = (unsigned char) (API_FP_MM_SET_REGISTRATION_MODE_REQ&0x00ff);      // Primitive LSB */
-/*          *(tempPtr+4) = 1; //enable registration */
-
-/*          /\* Start the registration timer *\/ */
-/*          bosTimeGetMs( &dectRegTimeout ); */
-
-/*          /\* Attempt to open the registration window. */
-/*          *\/ */
-/*          regWndOpening = TRUE; */
-/*       } */
-/*       break; */
-
-/*       case DECT_REGCMD_STOP_REG: */
-/*       { */
-/*          tempPtr = (unsigned char*) malloc(5); */
-/*          if (tempPtr == NULL) */
-/*          { */
-/*             cmsLog_error("No more memory!!!"); */
-/*             return; */
-/*          } */
-/*          *(tempPtr+0) = 0; // Length MSB */
-/*          *(tempPtr+1) = 3; // Length LSB */
-/*          *(tempPtr+2) = (unsigned char) ((API_FP_MM_SET_REGISTRATION_MODE_REQ&0xff00)>>8); // Primitive MSB */
-/*          *(tempPtr+3) = (unsigned char) (API_FP_MM_SET_REGISTRATION_MODE_REQ&0x00ff);      // Primitive LSB */
-/*          *(tempPtr+4) = 0; //disable registration */
-
-/*          /\* Stop the registration timer *\/ */
-/*          dectRegTimeout = 0; */
-
-/*          /\* Attempt to close the registration window. */
-/*          *\/ */
-/*          regWndOpening = FALSE; */
-/*       } */
-/*       break; */
-
-   /*    case DECT_REGCMD_GET_REG_COUNT: */
-   /*    { */
-   /*       tempPtr = (unsigned char*) malloc(4); */
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-   /*       *(tempPtr+0) = 0; // Length MSB */
-   /*       *(tempPtr+1) = 2; // Length LSB */
-   /*       *(tempPtr+2) = (unsigned char) ((API_FP_MM_GET_REGISTRATION_COUNT_REQ&0xff00)>>8); // Primitive MSB */
-   /*       *(tempPtr+3) = (unsigned char) (API_FP_MM_GET_REGISTRATION_COUNT_REQ&0x00ff);      // Primitive LSB */
-   /*    } */
-   /*    break; */
-
-   /*    case DECT_REGCMD_SET_ACCESS_CODE: */
-   /*    { */
-   /*       if (length != 4 ) */
-   /*       { */
-   /*          cmsLog_error("Access code shoule be 4 digits!!! Input access code is %s\n", dataPtr); */
-   /*       } */
-
-   /*       tempPtr = (unsigned char*) malloc(6); */
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-
-   /*       *(tempPtr+0) = 0; // Length MSB */
-   /*       *(tempPtr+1) = 4; // Length LSB */
-   /*       *(tempPtr+2) = (unsigned char) ((API_FP_MM_SET_ACCESS_CODE_REQ&0xff00)>>8); // Primitive MSB */
-   /*       *(tempPtr+3) = (unsigned char) (API_FP_MM_SET_ACCESS_CODE_REQ&0x00ff);      // Primitive LSB */
-   /*       *(tempPtr+4) = ((dataPtr[0]&0x0F) <<4); */
-   /*       *(tempPtr+4) += (dataPtr[1]&0x0F); */
-   /*       *(tempPtr+5) = ((dataPtr[2]&0x0F) <<4); */
-   /*       *(tempPtr+5) += (dataPtr[3]&0x0F); */
-
-   /*       /\* Cache the new access code until we have confirmation it has been */
-   /*       ** successfully set. */
-   /*       *\/ */
-   /*       memset ( newAccessCode, 0, sizeof( newAccessCode ) ); */
-   /*       strncpy ( newAccessCode, */
-   /*                 (char *)dataPtr, */
-   /*                 ( length < DECT_AC_MAX_LEN ) ? length : DECT_AC_MAX_LEN  ); */
-   /*    } */
-   /*    break; */
-
-   /*    case DECT_REGCMD_GET_ACCESS_CODE: */
-   /*    { */
-   /*       tempPtr = (unsigned char*) malloc(4); */
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-   /*       *(tempPtr+0) = 0; // Length MSB */
-   /*       *(tempPtr+1) = 2; // Length LSB */
-   /*       *(tempPtr+2) = (unsigned char) ((API_FP_MM_GET_ACCESS_CODE_REQ&0xff00)>>8); // Primitive MSB */
-   /*       *(tempPtr+3) = (unsigned char) (API_FP_MM_GET_ACCESS_CODE_REQ&0x00ff);      // Primitive LSB */
-   /*    } */
-   /*    break; */
-
-   /*    case DECT_REGCMD_GET_ID: */
-   /*    { */
-   /*       tempPtr = (unsigned char*) malloc(4); */
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-   /*       *(tempPtr+0) = 0; // Length MSB */
-   /*       *(tempPtr+1) = 2; // Length LSB */
-   /*       *(tempPtr+2) = (unsigned char) ((API_FP_MM_GET_ID_REQ&0xff00)>>8); // Primitive MSB */
-   /*       *(tempPtr+3) = (unsigned char) (API_FP_MM_GET_ID_REQ&0x00ff);      // Primitive LSB */
-   /*    } */
-   /*    break; */
-
-   /*    case DECT_REGCMD_GET_FW_VERSION: */
-   /*    { */
-   /*       tempPtr = (unsigned char*) malloc(4); */
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-   /*       *(tempPtr+0) = 0; // Length MSB */
-   /*       *(tempPtr+1) = 2; // Length LSB */
-   /*       *(tempPtr+2) = (unsigned char) ((API_FP_GET_FW_VERSION_REQ&0xff00)>>8); // Primitive MSB */
-   /*       *(tempPtr+3) = (unsigned char) (API_FP_GET_FW_VERSION_REQ&0x00ff);      // Primitive LSB */
-   /*    } */
-   /*    break; */
-
-   /*    case DECT_REGCMD_GET_MODEL: */
-   /*    { */
-   /*       tempPtr = (unsigned char*) malloc(4); */
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-   /*       *(tempPtr+0) = 0; // Length MSB */
-   /*       *(tempPtr+1) = 2; // Length LSB */
-   /*       *(tempPtr+2) = (unsigned char) ((API_FP_MM_GET_MODEL_REQ&0xff00)>>8); // Primitive MSB */
-   /*       *(tempPtr+3) = (unsigned char) (API_FP_MM_GET_MODEL_REQ&0x00ff);      // Primitive LSB */
-   /*    } */
-   /*    break; */
-
-   /*    case DECT_REGCMD_GET_HSET_IPUI: */
-   /*    { */
-   /*       if ( length < 1 ) */
-   /*       { */
-   /*          cmsLog_error("No handset is specify to retrieve IPUI!"); */
-   /*       } */
-
-   /*       tempPtr = (unsigned char*) malloc(5); */
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-
-   /*       *(tempPtr+0) = 0; // Length MSB */
-   /*       *(tempPtr+1) = 3; // Length LSB */
-   /*       *(tempPtr+2) = (unsigned char) ((API_FP_MM_GET_HANDSET_IPUI_REQ&0xff00)>>8); // Primitive MSB */
-   /*       *(tempPtr+3) = (unsigned char) (API_FP_MM_GET_HANDSET_IPUI_REQ&0x00ff);      // Primitive LSB */
-   /*       *(tempPtr+4) = (unsigned char) dataPtr[0];      // Handset Id */
-   /*    } */
-   /*    break; */
-
-   /*    case DECT_REGCMD_SYNC_DATE_TIME: */
-   /*    { */
-   /*       unsigned short mailSize = sizeof(ApiFpSetTimeReqType); */
-   /*       unsigned char * mailPtr = NULL; */
-
-   /*       time_t currentTime; */
-
-   /*       // get current time */
-   /*       time (&currentTime); */
-
-   /*       struct tm * ptm= localtime(&currentTime); */
-
-   /*       cmsLog_debug("Current time is: %d-%d-%d %d:%d:%d \n", */
-   /*                     (ptm->tm_year) + 1900, */
-   /*                     (ptm->tm_mon)+1, */
-   /*                     ptm->tm_mday, */
-   /*                     ptm->tm_hour, */
-   /*                     ptm->tm_min, */
-   /*                     ptm->tm_sec); */
-
-   /*       tempPtr = (unsigned char*) malloc(mailSize + 2); */
-   /*       mailPtr = tempPtr + 2; */
-
-   /*       if (tempPtr == NULL) */
-   /*       { */
-   /*          cmsLog_error("No more memory!!!"); */
-   /*          return; */
-   /*       } */
-
-   /*       *(tempPtr+0) = (unsigned char)(mailSize >> 8);     // Length MSB */
-   /*       *(tempPtr+1) = (unsigned char)(mailSize & 0x00FF); // Length LSB */
-
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->Primitive = API_FP_SET_TIME_REQ; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->Coding = API_CODING_TIME_DATE; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->Interpretation = API_INTER_CURRENT_TIME_DATE; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->ApiTimeDateCode.Year = ptm->tm_year; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->ApiTimeDateCode.Month = (ptm->tm_mon)+1; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->ApiTimeDateCode.Day = ptm->tm_mday; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->ApiTimeDateCode.Hour = ptm->tm_hour; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->ApiTimeDateCode.Minute = ptm->tm_min; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->ApiTimeDateCode.Second = ptm->tm_sec; */
-   /*       ((ApiFpSetTimeReqType *)mailPtr)->ApiTimeDateCode.TimeZone = 0; //TBD. To get the timezone from the system. */
-   /*    } */
-   /*    break; */
-
-   /*    default: */
-   /*    break; */
-   /* } */
-
-/* if (tempPtr != NULL) */
-/*    { */
-/*       /\* bosMutexAcquire(&semMailFrameQueueSem); *\/ */
-/*       /\* EnQueue(&mailFrameQueue, tempPtr); *\/ */
-/*       /\* bosMutexRelease(&semMailFrameQueueSem); *\/ */
-
-/*      unsigned short bSenderSize = 256*tempPtr[0] + tempPtr[1]; */
-
-/*      dectDrvWrite((void *)(tempPtr+2), bSenderSize); */
-/*    } */
-/* } */
 
 
 
@@ -575,11 +157,43 @@ static void dectNvsCtlGetData( unsigned char *pNvsData )
 
 
 
+
+
+EPSTATUS vrgEndptConsoleCmd( ENDPT_STATE *endptState, EPCONSOLECMD cmd, EPCMD_PARMS *consoleCmdParams )
+{
+   ENDPOINTDRV_CONSOLE_CMD_PARM tConsoleParm;
+   int fileHandle;
+
+   fileHandle = open("/dev/bcmendpoint0", O_RDWR);
+
+   tConsoleParm.state      = endptState;
+   tConsoleParm.cmd        = cmd;
+   tConsoleParm.lineId     = endptState->lineId;
+   tConsoleParm.consoleCmdParams = consoleCmdParams;
+   tConsoleParm.epStatus   = EPSTATUS_DRIVER_ERROR;
+   tConsoleParm.size       = sizeof(ENDPOINTDRV_CONSOLE_CMD_PARM);
+
+   if ( ioctl( fileHandle, ENDPOINTIOCTL_ENDPT_CONSOLE_CMD, &tConsoleParm ) != IOCTL_STATUS_SUCCESS )
+   {
+      printf(("%s: error during ioctl", __FUNCTION__));
+   }
+
+   close(fileHandle);
+
+   return( tConsoleParm.epStatus );
+}
+
+
+
+
 static int dect_init(void)
 {
   int fd, r;
   ApiFpLinuxInitReqType *t = NULL;
   DECTSHIMDRV_INIT_PARAM parm;
+  ENDPT_STATE    endptState;
+  EPCMD_PARMS    consoleCmdParams;
+
 
   fd = open("/dev/dectshim", O_RDWR);
   
@@ -601,6 +215,21 @@ static int dect_init(void)
   t->Primitive = API_FP_LINUX_INIT_REQ;
   dectNvsCtlGetData(t->NvsData);
   dectDrvWrite(t, sizeof(ApiFpLinuxInitReqType));
+
+
+
+  /* initialize dect procesing in dect enpoint driver */
+  memset( &consoleCmdParams,0, sizeof(consoleCmdParams) );
+  memset( &endptState, 0, sizeof(endptState) );
+
+  endptState.lineId = 0;
+
+  /* if ( vrgEndptGetNumDectEndpoints() > 0 ) */
+  /*   { */
+  vrgEndptConsoleCmd( &endptState,
+		      EPCMD_DECT_START_BUFF_PROC,
+		      &consoleCmdParams );
+  /* } */
   
   return r;
 }
