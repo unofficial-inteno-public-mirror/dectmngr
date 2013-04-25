f7e5292da6a749f3a0109a98b8eebb86f204643f
Jonas Höglund <jonas.hoglund@embeddednation.com>
2013-04-25 14:46:31 +0200

    Command line parsing

--- dect.c
+++ dect.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <netinet/in.h>
+#include <getopt.h>
 
 #include "dectd.h"
 
@@ -91,16 +92,54 @@ int establish_connection(void) {
 	return s;
 }
 
+#define ACTIVATE_REG 1 << 0
+#define DELETE_HSET  1 << 1
+#define PING_HSET    1 << 2
+#define GET_STATUS   1 << 3
 
-int main(void) {
 
-	int s;
+int main(int argc, char *argv[]) {
+
+	int s, c, flags;
 
 	s = establish_connection();
 
-	set_registration(s, ENABLED);
+	/* Parse command line options */
+	while ((c = getopt (argc, argv, "rdps")) != -1) {
+		switch (c) {
+		case 'r':
+			flags |= ACTIVATE_REG;
+			break;
+		case 'd':
+			flags |= DELETE_HSET;
+			break;
+		case 'p':
+			flags |= PING_HSET;
+			break;
+		case 's':
+			flags |= GET_STATUS;
+			break;
+	
+		}
+	}
 
-	set_registration(s, DISABLED);
+	if (flags & ACTIVATE_REG) {
+		set_registration(s, ENABLED);
+
+		set_registration(s, DISABLED);
+	}
+
+	if (flags & DELETE_HSET) {
+		printf("delete hset\n");
+	}
+
+	if (flags & PING_HSET) {
+		printf("ping hset\n");
+	}
+
+	if (flags & GET_STATUS) {
+		printf("get status\n");
+	}
 	
 	return 0;
 }
