89f327bd347c064e57757a292b657f26c4da06c3
Jonas Höglund <jonas.hoglund@embeddednation.com>
2014-10-01 11:28:58 +0200

    Disable registration after handset is registered

--- dectmngr.c
+++ dectmngr.c
@@ -1061,6 +1061,7 @@ void handle_dect_packet(unsigned char *buf) {
 	case API_FP_MM_HANDSET_PRESENT_IND:
 		printf("API_FP_MM_HANDSET_PRESENT_IND\n");
 		present_ind(buf);
+		list_handsets();
 		break;
 
 	case API_FP_MM_SET_REGISTRATION_MODE_CFM:
@@ -1084,7 +1085,7 @@ void handle_dect_packet(unsigned char *buf) {
 
 	case API_FP_MM_REGISTRATION_COMPLETE_IND:
 		printf("API_FP_MM_REGISTRATION_COMPLETE_IND\n");
-		list_handsets();
+		register_handsets_stop();
 		break;
 
 	case API_LINUX_INIT_CFM:
